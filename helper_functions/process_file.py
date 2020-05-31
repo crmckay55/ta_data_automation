07a80a847c96618ce0db41b622987128c33ada1e
Chris <crmckay55@gmail.com>
2020-05-31 17:08:44 -0600

    updated with test functions

--- helper_functions/process_file.py
+++ helper_functions/process_file.py
@@ -1,36 +1,35 @@
-import os, sys, importlib
-from os import listdir
+import os, importlib
 import logging
-import uuid
 import tempfile
-import json
-from bs4 import BeautifulSoup
-import pandas as pd
-from azure.cosmosdb.table.tableservice import TableService
-from azure.cosmosdb.table.models import EntityProperty, EdmType
+
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from . import storage_table
+try:
+    from . import storage_table
+    from . import config
+except:
+    from helper_functions import storage_table
+    from helper_functions import config
 
 
 def process_blob(blob_name):
+
+    az_config = config.DefaultConfig()
 
     logging.info(f'Started process_blob with {blob_name}.')
 
     partition_key = _get_partition_key(blob_name)
     row_prefix = _get_row_key_prefix(blob_name)
     file_name = blob_name
-    storage_blob_container = os.getenv('StorageBlobContainer')
     logging.info(f'Set PartitionKey to {partition_key}.')
 
-    connect_str = os.getenv('AzureWebJobsStorage')
-    blob_services_client = BlobServiceClient.from_connection_string(connect_str)
-    blob_container_client = blob_services_client.get_container_client(storage_blob_container)
+    blob_services_client = BlobServiceClient.from_connection_string(az_config.STORAGE_CONNECTION)
+    blob_container_client = blob_services_client.get_container_client(az_config.BLOB_CONTAINER)
     blob_client = blob_container_client.get_blob_client(file_name)
     logging.info(f'created blob_client for {file_name}')
 
     # load new blob into an object
-    tempFilePath = tempfile.gettempdir()
-    temp_file = os.path.join(tempFilePath, 'file.htm')
+    temp_file_path = tempfile.gettempdir()
+    temp_file = os.path.join(temp_file_path, 'file.htm')
     logging.info(f'Ready to save to {temp_file}')
 
     with open(temp_file, 'wb') as f:
@@ -53,7 +52,6 @@ def process_blob(blob_name):
     except ModuleNotFoundError:
         transaction_processor = importlib.import_module(f'__app__.helper_functions.{transaction}', package=None)
         logging.info(f'Found module for {transaction} with __app__')
-
 
     parsed_df = transaction_processor.process_data(contents, partition_key, row_prefix)
     
