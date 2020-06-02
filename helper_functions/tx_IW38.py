1c5b9d394d4befb8eed58288c19c3e5e75315d70
crmckay55 <crmckay55@gmail.com>
2020-06-01 19:56:44 -0600

    update

--- helper_functions/tx_IW38.py
+++ helper_functions/tx_IW38.py
@@ -103,3 +103,58 @@ def _IW38_01_get_row(columns, headers):
         current_row[headers[column_idx]] = value[idx].text.strip().replace('\xa0', ' ')
 
     return json.dumps(current_row)
+
+
+'''---------------------------------------
+ Version 2                                 
+ Same as version 1, but parses to a json
+ file instead for gen 2 storage, not
+ table storage       
+----------------------------------------'''
+
+'''
+def _IW38_02(blob_contents, partition_key, row_prefix):
+    # TODO: make this dump the columns into a normal dataframe format, and then dump the DF into a json itself with name of file the PartitionKey
+    # processes version 1 of IW38
+    # Doesn't matter how many columns.
+    # Accounts for the weird thing happening in the first column
+
+    df = pd.DataFrame(columns=['PartitionKey', 'RowKey', 'json_data'])
+
+    # parse contents
+    soup = BeautifulSoup(blob_contents, "html.parser")
+    logging.info('Parsed blob item with BeautifulSoup')
+
+    # strip out lists only
+    stripped_lists = soup('table', {"class": "list"})
+    logging.info('Stripped out the lists.')
+
+    # iterate through lists to get bodies
+    for idx_lst, lst in enumerate(stripped_lists):
+        stripped_bodies = lst('tbody')
+
+        # iterate through bodies to get rows
+        for idx_body, body in enumerate(stripped_bodies):
+
+            # if first list and body, get header rows
+            if idx_lst == 0 and idx_body == 0:
+                hdr_row = body('tr')
+                columns = hdr_row[0]('td')
+                keys = _IW38_01_set_headers(columns)
+
+            # if not first list, skip body 0 which is the header again
+            elif idx_body > 0:
+                stripped_rows = body('tr')
+
+                # iterate through rows and get columns
+                for idx_row, row in enumerate(stripped_rows):
+                    columns = row('td')
+
+                    df = df.append({'PartitionKey': partition_key,
+                                    'RowKey': row_prefix + str(len(df) + 1),
+                                    'json_data': _IW38_01_get_row(columns, keys)},
+                                   ignore_index=True)
+
+    logging.info('Finished parsing the file into a dataframe.')
+    return df
+    '''
--- test/test_blob.py
+++ test/test_blob.py
@@ -2,5 +2,5 @@
 
 import test.debug_function as qt
 
-qt.main('Job SAP-IW38_01-CARSELAND 2021-20200530, Step 1.htm')
+qt.main('Job SAP-IW38_01-CARSELAND 2021-20200531, Step 1.htm')
 
